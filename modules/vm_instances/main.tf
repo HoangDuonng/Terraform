c42b0a7d9064ac8e78a96c2c1d2d7a0d8f99e1f9
HoangDuonng <Nguuyen1801@gmail.com>
2024-12-14 00:58:42 +0700

    setup(server): install tools via .sh script

--- modules/vm_instances/main.tf
+++ modules/vm_instances/main.tf
@@ -5,7 +5,7 @@ resource "google_compute_instance" "vm_instance" {
 
   boot_disk {
     initialize_params {
-      image = "debian-cloud/debian-11"
+      image = var.image
       size  = var.disk_size
     }
   }
@@ -17,4 +17,43 @@ resource "google_compute_instance" "vm_instance" {
       # Ephemeral public IP
     }
   }
+
+  metadata_startup_script = file("docker-install.sh")
+  # <<-EOF
+  #   #!/bin/bash
+  #   echo "Starting setup..." > /tmp/startup.log
+  #   mkdir -p /tmp/scripts
+  #   echo "${file("docker-install.sh")}" > /tmp/scripts/docker-install.sh
+  #   echo "${file("jenkins-install.sh")}" > /tmp/scripts/jenkins-install.sh
+  #   echo "${file("harbor-install.sh")}" > /tmp/scripts/harbor-install.sh
+  #   chmod +x /tmp/scripts/*.sh
+  #   /tmp/scripts/docker-install.sh >> /tmp/startup.log 2>&1
+  #   /tmp/scripts/jenkins-install.sh >> /tmp/startup.log 2>&1
+  #   /tmp/scripts/harbor-install.sh >> /tmp/startup.log 2>&1
+  #   echo "Setup completed!" >> /tmp/startup.log
+  # EOF
+
+
 }
+
+# resource "null_resource" "test" {
+#     provisioner "local-exec" {
+#     command = "echo The server's IP address is ${self.private_ip}"
+#   }
+# }
+
+# resource "null_resource" "harbor_install" {
+#    provisioner "local-exec" {
+    
+#     command =   "bash ${path.module}/harbor-install.sh"
+    
+#   }
+# }
+
+# resource "null_resource" "jenkins_install" {
+#   provisioner "local-exec" {
+   
+#     command =  "bash ${path.module}/jenkins-install.sh"
+    
+#   }
+# }
